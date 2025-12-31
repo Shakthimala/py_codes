7b613414d92c69c981d1729c2949983ea7823fe7
shakthimalaa <cb.en.u4cse22053@cb.students.amrita.edu>
2025-12-31 16:13:23 +0530

    feat: optimised solution for nextPermutation

---
+++ codes/nextPermutaton_optimised.py
@@ -0,0 +1,27 @@
+from typing import List
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        ind = -1
+        for i in range(n-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                ind = i
+                break
+        if ind == -1:
+            nums.reverse()
+            return
+        for i in range(n-1,ind,-1):
+            if nums[i]>nums[ind]:
+                nums[ind], nums[i] = nums[i], nums[ind]
+                break
+        nums[ind + 1:] = reversed(nums[ind + 1:])
+        return nums
+    
+if __name__ == "__main__":
+    nums = [3,1,2]
+    sol = Solution()
+    print(sol.nextPermutation(nums))
+    
